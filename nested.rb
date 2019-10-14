0c5ec5f661f5966e5315013c88c343e3dd3918ba
Brittney Haselmayer <brittneyhaselmayer@yahoo.com>
2019-10-14 12:33:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,7 +128,7 @@ def adding_to_dennis
         }
      }
      
-     adding_to_dennis[:dennis_ritchie][:languages].push("Assembly")
-     return adding_to_dennis
+     programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+     return programmer_hash
 
 end
